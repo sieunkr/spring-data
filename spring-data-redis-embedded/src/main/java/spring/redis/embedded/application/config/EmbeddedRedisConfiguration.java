f3f504de4475599e12e39c69d45f8d30c857b8b8
sieunkim <sieunkr@gmail.com>
2018-07-07 15:14:05 +0900

    Spring Data Redis 임베디드 로컬 구성 샘플

---
+++ spring-data-redis-embedded/src/main/java/spring/redis/embedded/application/config/EmbeddedRedisConfiguration.java
@@ -0,0 +1,30 @@
+package spring.redis.embedded.application.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import redis.embedded.RedisServer;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.io.IOException;
+
+@Component
+public class EmbeddedRedisConfiguration {
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void startRedis() throws IOException {
+        redisServer = new RedisServer(redisPort);
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void stopRedis() {
+        redisServer.stop();
+    }
+
+}
---
+++ spring-data-redis-embedded/src/main/java/spring/redis/embedded/application/config/RedisConfig.java
@@ -0,0 +1,50 @@
+package spring.redis.embedded.application.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.redis.database}")
+    private int redisDatabase;
+
+    /**
+     * Factory
+     *
+     * @return
+     */
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory() {
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
+        jedisConnectionFactory.setHostName(redisHost);
+        jedisConnectionFactory.setPort(redisPort);
+        jedisConnectionFactory.setDatabase(redisDatabase);
+        jedisConnectionFactory.setUsePool(true);
+        return jedisConnectionFactory;
+    }
+
+    /**
+     * redis Template
+     *
+     * @return
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+
+        return redisTemplate;
+    }
+}
