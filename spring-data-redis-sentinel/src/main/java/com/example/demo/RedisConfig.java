63c1c377c9c9fe93ebcb623c859aef377c20f6b3
Eddy.Kim <sieunkr@gmail.com>
2019-02-12 22:39:40 +0900

    Update RedisConfig.java

--- spring-data-redis-sentinel/src/main/java/com/example/demo/RedisConfig.java
+++ spring-data-redis-sentinel/src/main/java/com/example/demo/RedisConfig.java
@@ -3,34 +3,43 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisNode;
 import org.springframework.data.redis.connection.RedisSentinelConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 
 @Configuration
-//@PropertySource("/properties/redis.properties")
 public class RedisConfig {
-
 
     @Value("${redis.master}")
     private String redisMaster;
 
-    @Value("${redis.sentinels.host}")
-    private String redisHost;
+    @Value("${redis.password}")
+    private String password;
 
-    @Value("${redis.sentinels.port}")
-    private Integer redisPort;
+    @Value("${redis.sentinel.nodes}")
+    private String redisNodes;
 
 
     @Bean
     public RedisConnectionFactory jedisConnectionFactory() {
 
-        RedisSentinelConfiguration sentinelConfig = new RedisSentinelConfiguration().master(redisMaster)
-                .sentinel(redisHost, redisPort);
+        RedisSentinelConfiguration sentinelConfig = new RedisSentinelConfiguration().master(redisMaster);
 
-        return new JedisConnectionFactory(sentinelConfig);
+        String[] host = redisNodes.split(",");
+        for(String redisHost : host){
+            String[] item = redisHost.split(":");
+            String ip = item[0];
+            String port = item[1];
+            sentinelConfig.addSentinel(new RedisNode(ip, Integer.parseInt(port)));
+        }
+
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(sentinelConfig);
+        jedisConnectionFactory.setPassword(password);
+
+        return jedisConnectionFactory;
     }
 
 }
-
