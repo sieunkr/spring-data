5ec2f71d57e05830dbfd054ff26a2546ac98c178
Eddy.Kim <sieunkr@gmail.com>
2019-07-07 21:16:03 +0900

    수정 버전..

--- spring-data-redis-devtools-error/src/main/java/com/example/demo/RedisConfig.java
+++ spring-data-redis-devtools-error/src/main/java/com/example/demo/RedisConfig.java
@@ -3,9 +3,12 @@ package com.example.demo;
 import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 import java.time.Duration;
 
@@ -13,10 +16,21 @@ import java.time.Duration;
 public class RedisConfig {
 
     @Bean
-    public CacheManager cacheManager(RedisConnectionFactory connectionFactory){
+    public CacheManager cacheManager(RedisConnectionFactory connectionFactory,
+                                     ResourceLoader resourceLoader){
 
-        RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofMinutes(5));
+        RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration
+                .defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10));
+
+        cacheConfiguration = cacheConfiguration.serializeValuesWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(
+                        new JdkSerializationRedisSerializer(resourceLoader.getClassLoader())));
+
+
+        /*
+        비즈니스 관련 로직이 추가되어 있다.
+        */
 
         return RedisCacheManager.builder(connectionFactory)
                 .cacheDefaults(cacheConfiguration)
